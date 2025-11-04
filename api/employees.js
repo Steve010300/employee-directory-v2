6aff807b6f3d973309205aba62cb85085fb378d6
Steve010300 <stleonard978@gmail.com>
2025-11-03 21:10:59 -0500

    Updated all employees to be in there own route

--- api/employees.js
+++ api/employees.js
@@ -1,30 +1,76 @@
 import express from "express";
-const router = express.Router();
-export default router;
+const employeesRouter = express.Router();
+export default employeesRouter;
 
-import { getEmployees, getEmployeeById, addEmployee } from "../db/employees";
+import employees, { getEmployees, getEmployeeById, addEmployee } from "../db/employees.js";
 
-router.get("/", (req, res) => {
-  const employees = getEmployees();
-  res.send(employees);  
+
+employeesRouter.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+employeesRouter
+  .route("/")
+  .get((request, response) => {
+    response.send(getEmployees());
+  })
+  .post((req, res) => {
+    if (!req.body) {
+      return res.status(400).send("Request must have a body.");
+    }
+
+    const { name } = req.body;
+    if (!(name)) {
+      return res.status(400).send("New employee must have name.")
+    }
+
+    const employee = addEmployee(name);
+    res.status(201).send(employee);
+  });
+
+  
+employeesRouter.route("/:id").get((req, res) => {
+  const { id } = req.params;
+  
+  const singleEmployee = getEmployeeById(+id);
+  
+  if (!singleEmployee) {
+    return res.status(404).send("Employee not found.");
+  }
+
+  res.send(singleEmployee);
 });
 
 
-router.post("/", (req, res) => {
-  if (!req.body) return res.status(400).send("Request must have a body.");
-  
-  const { name } = req.body;
-  if (!name) return res.status(400).send("New employee must have name.");
 
-  const employee = addEmployee(name);
-  res.status(201).send(employee);
-});
+// Note: this middleware has to come first! Otherwise, Express will treat
+// "random" as the argument to the `id` parameter of /employees/:id.
 
-// router.get("/:id", (req, res) => {
-//   const { id } = req.params;
-//   const employee = getEmployeeById(+id);
-  
-//   if (!employee) return res.status(404).send("Employee not found.");
 
-//   res.send(employee);
+
+
+// router.get("/", (req, res) => {
+//   const employees = getEmployees();
+//   res.send(employees);  
 // });
+
+
+// router.post("/", (req, res) => {
+//   if (!req.body) return res.status(400).send("Request must have a body.");
+  
+//   const { name } = req.body;
+//   if (!name) return res.status(400).send("New employee must have name.");
+
+//   const employee = addEmployee(name);
+//   res.status(201).send(employee);
+// });
+
+// // router.get("/:id", (req, res) => {
+// //   const { id } = req.params;
+// //   const employee = getEmployeeById(+id);
+  
+// //   if (!employee) return res.status(404).send("Employee not found.");
+
+// //   res.send(employee);
+// // });
